b615d0308bab00b4e7c0c6943aec39bc167f7b6d
rishabh-aaryan <acompsciproject@gmail.com>
2020-11-30 22:59:29 +0530

    BLOG

--- front.py
+++ front.py
@@ -1,3 +1,5 @@
+from tkinter import * 
+from tkinter import filedialog, Text, font
 import sys
 import timeit
 import os
@@ -6,56 +8,37 @@ from datetime import datetime
 import pickle
 
 
-def alter(user):
-	file_name = open("preferences.dat", "rb") 
-	data = pickle.load(file_name)
-	for i in data:
-		if(i==user):
-			user_data = data[i]
-			break
-	
-	print("Current tags:\n", user_data[0])
-	
-	tag = input("Which tag would you want to change?: ")
-	idx = 0
-	loc = -1
-	for i in user_data[0]:
-		if(i==tag):
-			loc = idx
-		idx += 1
-	
-	if(loc == -1):
-		print("Invalid tag please try again.")
-	else:
-		new_tag = input("Enter new tag: ")
-		user_data[0][loc] = new_tag
-		print("New tag list:", user_data[0])
-		print("Operation successful")
-		data[i] = user_data
-		file_name.close()
-		file_name = open("preferences.dat", "wb")
-		pickle.dump(data, file_name)
-		file_name.close()
-			
-	menu(user)
-		
 
-def publish(user):
+
+
+def checkex(tag):
+	write_to = tag
+	print(path.exists(write_to+'.html'))
+	if(path.exists(write_to+'.html')==False):
+		return 0
+	else:
+		return 1
+
+def publish4(user, texx, titl, taglist):
+	
+
 	start = timeit.default_timer() 
 
 	now = datetime.now()
 	dt_string = now.strftime("%d/%m/%Y %H:%M")
-	title = input("Enter title")
+	title = titl[0:len(titl)-1]
 
-	tags = eval(input("enter the tags you want to put the blog in ['tag1', 'tag2']: "))
+	print("here", taglist)
+	tags = eval(taglist)
+
 	for z in range(0, len(tags)):
 	
-	    write_to = tags[z] 
-	
-    
+	    write_to = tags[z]
+	    #getdesc()
+	    #print(desc)
+	    desc='f'
 	    print(path.exists(write_to+'.html'))
 	    if(path.exists(write_to+'.html')==False):
-	        desc = input("Enter new board's description")
 	        index = open("index.html", "r")
 	        ch = 1
 	        tmp = tmp2 = tmp3 = ''
@@ -69,7 +52,7 @@ def publish(user):
 	
 	        index = open("index.html", "w")
   	      
-	        tmp = tmp3 + '<div class = "entry"><p><a href="'+write_to+'.html">'+write_to+'</a>:'+desc+'</p></div>' + tmp2
+	        tmp = tmp3 + '<div class = "entry"><p><a href="'+write_to+'.html">'+write_to+'</a></p></div>' + tmp2
 	
 	        html = open(write_to+'.html', 'w')
 	        index.write(tmp)
@@ -97,11 +80,11 @@ def publish(user):
 	        if(i==18):
 	            html.close()
 	            html = open(write_to, "w")
-	            text = main.read()
+	            text = texx
 	            s1 = '<h2 id=\''+title+'\'>'+ title + '</h2><small>[<a href="blog/'+title+'.html">standalone</a>]</small>'
 	            s1 += '<div class = "tag"><small><b>Tags: </b>'
-	            for q in range(1, len(sys.argv)):
-	                s1 += sys.argv[q] + ' '
+	            for q in range(0, len(tags)):
+	                s1 += tags[q] + ' '
 	            s1 +='</small></div><p>' + text + '</p> <small>' + dt_string + ' ' + datetime.today().strftime('%A') + '</small>'
 	            s1 = '<div class = "entry">' + '\n' + s1 + '</div>'
 	
@@ -120,210 +103,171 @@ def publish(user):
 	os.system(cmd)
 	
 	stop = timeit.default_timer()
-	print('Operation Successful. Time taken: ', stop - start) 
+	return 'Operation Successful. Time taken: ', stop - start
 	
 
 
 
 
-def menu(user):
-	#the core part :D
-	#all the options and preferences will be here
-	#lets start out slow and put the basics here first
-	ch = int(input('''
-	1. Publish blog(everything written in blog.txt)
-	2. Alter default tags
-	:'''))
-	if(ch==1):
-		publish(user)
-	elif(ch==2):
-		alter(user)
-	else:
-		exit()
 
-
-def set_preferences(user):
-	old_tags = ['<b>','<strong>','<i>','<em>','<mark>','<small>','<del>','<ins>','<sub>','<sup>']
-	old_tags.sort()
-	file_name = open("preferences.dat", "rb")
-	try:
-		data = pickle.load(file_name)
-		tags = list()
-		tags.append(old_tags)
-		tags.append(old_tags) #new_tags
-		data[user] = tags
-		file_name.close()
-		file_name = open("preferences.dat", "wb")
-		pickle.dump(data, file_name)
-		file_name.close()
-	except:
-		file_name.close()
-		tags = list()
-		tags.append(old_tags)
-		tags.append(old_tags) #new_tags
-		data = dict()
-		data[user] = tags
-		file_name = open("preferences.dat", "wb")
-		pickle.dump(data, file_name)        
-		file_name.close()
-
-
-def make_default(user):
-	name = user + '.html'
-	index = open(name, "w")
-	data = open("default.html", "r")
-	lines = ''
-	for line in data:
-		lines += line
-	index.write(lines)
-	index.close()
-	data.close()  
+def gui():
 
 
 
+  global desc
+  def getdesc():
+ 		popup = Tk()
+ 		popup.geometry("500x150")
+ 		def leave():
+ 			popup.destroy()
+ 		def submit_desc():
+ 			desc = des.get("1.0", END)
+ 			print("WHOA", desc)
+ 			leave()
+ 		popup.wm_title("Enter description")
+ 		label = Label(popup, text='Enter new board\'s description', font=("Helvetica", 10))
+ 		label.pack(side="top",fill="x",pady=10)
+ 		des = Text(popup, width=27, height=2, font=("Helvetica", 14), undo=True,selectbackground="yellow"
+	  		, selectforeground="black")
+ 		des.pack()
+ 		b1 = Button(popup,text="Submit",command=submit_desc)
+ 		b1.pack()
+ 		popup.mainloop()
 
-def setup(user):
-	def1 = '''<html>
-
-<head>
-
-<title>The Simple Blog</title>
-
-<meta charset="utf-8"/>
-
-<link rel='stylesheet' type='text/css' href='style.css'>
-
-</head>
-
-<body style='font-family="Courier"'>
-	
-<h1 style="color:black">A Website(?)</h1>
-
-<p>Navigation</p>
-'''    
-	def2 = '''</body> </html>
-'''
-	d = dict()
-	#if not os.path.exists(os.path.dirname("./store.dat")):
-	f = open("style.css", "r")
-	s = f.read()
-	f.close()
-
-	pathb = os.path.dirname(os.path.abspath(__file__))
-	print(pathb)
-	path = pathb + '\\' + user #+"/style.css"
-
-	try:
-		os.makedirs(path)
-	except OSError:
-		print ("Creation of the directory %s failed" % path)
-	else:
-		print ("Successfully created the directory %s " % path)
-
-	f = open(path+"/style.css", "w")
-	f.write(s)
-	f.close()
-	try:
-		store = open("store.dat", "rb")
-		d = pickle.load(store)
-		store.close()
-		#os.makedirs(os.path.dirname("./store.dat"))
-		#store = open("store.dat", "wb")
-		#store.close()
-
-		store = open("store.dat", "wb")
-		l = list()
-		l.append(def1)
-		l.append(def2)
-		d[user] = l
-		pickle.dump(d, store)
-		store.close()
-
-	except:
-		store = open("store.dat", "wb")
-		l = list()
-		l.append(def1)
-		l.append(def2)
-		d[user] = l
-		pickle.dump(d, store)
-		store.close()
-	
-
-
-def signup(user):
-	file_name = open("user_data.dat", "rb")
-	try:
-		data = pickle.load(file_name)
-		if(data.get(user)):
-			print("username exists, please retry")
-			file_name.close()
-			return
-		else:
-			password = input("choose password: ")
-			data[user] = password
-			print("USER REGISTERED, please login now to use account")
-			file_name.close()
-			file_name = open("user_data.dat", "wb")
-			pickle.dump(data, file_name)
-			file_name.close()
-			set_preferences(user)
-			make_default(user)
-			#put_on_page(user)
-			setup(user)
-	except:   
-		data = dict()
-		password = input("choose password: ")
-		data[user] = password
-		print("USER REGISTERED, please login now to use account")
-		file_name = open("user_data.dat", "wb")
-		pickle.dump(data, file_name)
-		file_name.close()    
-		set_preferences(user)
-		make_default(user)
-		#put_on_page(user)
-		setup(user)
-	interface()
-			
-
-def login(user):
-	file_name = open("user_data.dat", "rb")
-	data = pickle.load(file_name)
-	#print(data)
-	password = input("enter password: ")
-	if(data.get(user) and data[user] == password):
-		print("LOGIN SUCCESSFUL")
-		#go on and do things
-		#basically interface #2 should start from here
-		#which will allow blog writing 
-		#will ask to enter blog name if opened for the first time
-		#then username and blogname will be published on the main page of the website(to be done later)
-		file_name.close()
-		menu(user) #WIP
-
-	elif(not data.get(user)):
-		print("please signup first")
-		file_name.close()
-		interface()
-	else:
-		print("PASSWORD DOES NOT MATCH PLEASE RETRY")
-		file_name.close()
-		interface()
+  def clear():
+  	my_text.delete(1.0, END)
 
 
 
-def interface():
-	ch = input("(s)ignup/ (l)ogin: ")
-	user = input("enter username: ")      
-	if(ch=='s'):
-		signup(user)
-	elif(ch=='l'):
-		login(user)
-	elif(ch=='setup'):
-		setup()
-	else:
-		#user made error show menu again
-		#interface()                        
-		exit() #for now, to avoid problems
+  def tags():
+  	popup = Tk()
+  	popup.geometry("500x150")
+  	def leave():
+  		popup.destroy()
+  	def submit_tags():
+  		global tagslist 
+  		tagslist = tags.get("1.0", END)
+  		print("WHOA", tagslist)
+  		leave()
+  	popup.wm_title("Enter tags")
+  	label = Label(popup, text='Enter Tags in the form [tag1,tag2,tag3]', font=("Helvetica", 10))
+  	label.pack(side="top",fill="x",pady=10)
+  	tags = Text(popup, width=27, height=2, font=("Helvetica", 14), undo=True,selectbackground="yellow"
+  		, selectforeground="black")
+  	tags.pack()
+  	b1 = Button(popup,text="Submit",command=submit_tags)
+  	b1.pack()
 
-interface() #login/signup
+  	popup.mainloop()
+
+  	
+  	return 0
+
+
+  def popupn(msg):
+  	def leave():
+  		popup.destroy()
+  		return
+  	popup = Tk()
+  	popup.wm_title()
+  	label = Label(popup, text=msg, font=("Helvetica", 10))
+  	label.pack(side="top",fill="x",pady=10)
+  	b1 = Button(popup, text="OK", command=leave)
+  	b1.pack()
+  	popup.mainloop()
+
+
+  def upl():
+  	text = my_text.get("1.0", END)
+  	title = my_title.get("1.0", END)
+  	tags()
+  	alist = eval(tagslist)
+  	'''
+  	for i in alist:
+  		there = checkex(i)
+  		if there==0:
+  			getdesc()
+  			#how do i get the description idk
+
+  	print(text, '\n', title, tagslist)
+  	'''
+  	ret_msg, time_taken = publish4('a', text, title, tagslist)
+  	popupn(ret_msg+str(time_taken))
+
+
+  def popupm(msg):
+  	popup = Tk()
+  	def leave():
+  		popup.destroy()
+  		return
+  	popup.wm_title("!")
+  	label = Label(popup, text='Username', font=("Helvetica", 10))
+  	label.pack(side="top",fill="x",pady=10)
+  	username = Text(popup, width=27, height=2, font=("Helvetica", 14), undo=True,selectbackground="yellow", selectforeground="black")
+  	username.pack()
+  	label = Label(popup, text='Password', font=("Helvetica", 10))
+  	label.pack(side="top",fill="x",pady=10)
+  	password = Text(popup, width=27, height=2, font=("Helvetica", 14), undo=True,selectbackground="yellow", selectforeground="black")
+  	password.pack()
+  	def log():
+  		reply = login2(username, password)
+  		print(reply)
+  		if(reply==1):
+  			popupn('Login Successful')
+  		else:
+  			popupn('Login Unsuccessful')
+  		leave()
+  	b1 = Button(popup, text="Log In",command=log)
+  	b1.pack()
+  	popup.mainloop()
+
+
+  root = Tk()
+  root.title('Blog maker')
+  root.geometry("1280x800")
+                              
+  my_frame = Frame(root)
+  my_frame.pack(pady=5)
+
+  w = Label(root, text='Title')
+  w.pack()
+
+  my_title = Text(root, width=97, height=2, font=("Helvetica", 14), undo=True,selectbackground="yellow", selectforeground="black")
+  my_title.pack()
+  
+  w = Label(root, text='Blog Text')
+  w.pack()
+
+  my_text = Text(root, width=97, height=25, font=("Helvetica", 14), undo=True, selectbackground="yellow", selectforeground="black")
+  my_text.pack()
+  
+  
+  
+  status_bar = Label(root, text = 'Ready    ', anchor=E) 
+  status_bar.pack(fill=X, side=BOTTOM, ipady=5)
+  
+  button_frame = Frame(root)
+  button_frame.pack()
+  
+  clear_button = Button(button_frame, text="Clear Screen", command=clear)
+  clear_button.grid(row=0, column=0)
+  
+  
+  my_label = Label(root, text='')
+  my_label.pack()
+  
+  
+  upload = Button(root, text = "Upload Blog", padx=15,pady=10,fg="white",bg="gray", command=upl)
+  upload.pack()
+  
+  #popupm('Please login')
+  
+  
+  root.mainloop()
+
+
+gui()
+
+
+
 
